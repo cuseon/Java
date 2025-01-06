b963f8a7c05a2b81ad86a84de11dc4c6ac98794d
cuseon <cuseon97@icloud.com>
2025-01-06 23:31:23 +0900

    Add files via upload

---
+++ java-start/src/variable/ex/VarEx3Question.java
@@ -0,0 +1,6 @@
+package variable.ex;
+
+public class VarEx3Question {
+    //long 타입의 변수를 선언하고. 그 변수를 백억으로 초기화한 후 출력하는 프로그램을 작성하시오
+    //boolean 타입의 변수를 선언하고, 그 변수를 true 로 초기화 한 후 출력하는 프로그램을 작성하시오.
+}
