e9e5404f845cc7a5436dfc175b2e108a7165abeb
cuseon <cuseon97@icloud.com>
2025-01-09 23:02:24 +0900

    Add files via upload

---
+++ src/cond/ex/ExchangeRateEx.java
@@ -0,0 +1,14 @@
+package cond.ex;
+
+public class ExchangeRateEx {
+    public static void main(String[] args) {
+        int dollor = 100;
+        if(dollor <0){
+            System.out.println("잘못된 금액입니다.");
+        }else if(dollor == 0) {
+            System.out.println("환전할 금액이 없습니다.");
+        }else{
+            System.out.println("환전할 금액은 " + (dollor * 1300)+"원 입니다." );
+        }
+    }
+}
